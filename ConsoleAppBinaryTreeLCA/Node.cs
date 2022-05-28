a0e74b894910762a45dc6c5f1b2532aa048bb88a
Alejandro Martinez <Alejandro.Martinez.-ND@disney.com>
2022-05-27 23:44:02 -0500

    first commit

---
+++ ConsoleAppBinaryTreeLCA/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBinaryTreeLCA
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public Node Child1 { get; set; }
+        public Node Child2 { get; set; }
+    }
+}
---
+++ ConsoleAppBinaryTreeLCA/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppBinaryTreeLCA;
+
+Console.WriteLine("Hello, World!");
+
+var bTree = new Node {
+    Id = 67,
+    Child1 = new Node {
+        Id=39,
+        Child1 = new Node
+        {
+            Id = 28,
+            Child1 = new Node
+            {
+                Id = 29
+            }
+        },
+        Child2 = new Node
+        {
+            Id = 44
+        }
+    },
+    Child2 = new Node {
+        Id = 76,
+        Child1 = new Node
+        {
+            Id = 74
+        },
+        Child2 = new Node
+        {
+            Id = 85,
+            Child1 = new Node
+            {
+                Id = 83
+            },
+            Child2 = new Node
+            {
+                Id = 87
+            }
+        }
+    }
+};
+
+
