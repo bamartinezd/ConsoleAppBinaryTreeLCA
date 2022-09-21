48635b453dc3e452d182f3b80ae6658a009f0f13
Alejandro Martinez <Alejandro.Martinez.-ND@disney.com>
2022-09-20 22:52:08 -0500

    Adding new method to get the LCA

--- ConsoleAppBinaryTreeLCA/Program.cs
+++ ConsoleAppBinaryTreeLCA/Program.cs
@@ -1,44 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleAppBinaryTreeLCA;
+using Newtonsoft.Json;
 
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-var bTree = new Node {
-    Id = 67,
-    Child1 = new Node {
-        Id=39,
-        Child1 = new Node
+        var bTree = new Node
         {
-            Id = 28,
+            Id = 67,
             Child1 = new Node
             {
-                Id = 29
-            }
-        },
-        Child2 = new Node
-        {
-            Id = 44
-        }
-    },
-    Child2 = new Node {
-        Id = 76,
-        Child1 = new Node
-        {
-            Id = 74
-        },
-        Child2 = new Node
-        {
-            Id = 85,
-            Child1 = new Node
-            {
-                Id = 83
+                Id = 39,
+                Child1 = new Node
+                {
+                    Id = 28,
+                    Child1 = new Node
+                    {
+                        Id = 29
+                    }
+                },
+                Child2 = new Node
+                {
+                    Id = 44
+                }
             },
             Child2 = new Node
             {
-                Id = 87
+                Id = 76,
+                Child1 = new Node
+                {
+                    Id = 74
+                },
+                Child2 = new Node
+                {
+                    Id = 85,
+                    Child1 = new Node
+                    {
+                        Id = 83
+                    },
+                    Child2 = new Node
+                    {
+                        Id = 87
+                    }
+                }
             }
-        }
+        };
+
+        Console.WriteLine(JsonConvert.SerializeObject(bTree, Formatting.Indented));
     }
-};
+
+    private Node LowestCommonAncestor(Node tree, int NodeLeft, int NodeRight){
+        //LowestCommonAncestor(tree, 83, 87) = 85
+
+        
+        return new Node();
+    }
+}
 
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ConsoleAppBinaryTreeLCA
 Obtaining the Lowest Common Ancestor of a node in a Binary Tree.
+
+LowestCommonAncestor(tree, 29, 44) = 39
+LowestCommonAncestor(tree, 44, 85) = 67
+LowestCommonAncestor(tree, 83, 87) = 85
