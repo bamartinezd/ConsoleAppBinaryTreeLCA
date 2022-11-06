1589832f92fdf547fa03183b29f203cd5fd145eb
Alejandro Martinez <Alejandro.Martinez.-ND@disney.com>
2022-11-05 22:25:43 -0500

    Implementation.

--- ConsoleAppBinaryTreeLCA/Program.cs
+++ ConsoleAppBinaryTreeLCA/Program.cs
@@ -1,13 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ConsoleAppBinaryTreeLCA;
-using Newtonsoft.Json;
 
 public class Program
 {
     public static void Main(String[] args)
     {
-        Console.WriteLine("Hello, World!");
-
         var bTree = new Node
         {
             Id = 67,
@@ -48,15 +45,26 @@ public class Program
                 }
             }
         };
-
-        Console.WriteLine(JsonConvert.SerializeObject(bTree, Formatting.Indented));
+        //Console.Write(JsonConvert.SerializeObject(bTree, Formatting.Indented));
+        LowestCommonAncestor(bTree, 83, 87); //85
     }
 
-    private Node LowestCommonAncestor(Node tree, int NodeLeft, int NodeRight){
-        //LowestCommonAncestor(tree, 83, 87) = 85
+    private static void LowestCommonAncestor(Node node, int NodeLeft, int NodeRight){
+        if (node.Id != NodeLeft)
+        {
+            Console.WriteLine($"This node: {node.Id}");
+            if (node.Child1 is not null)
+            {
+                Console.WriteLine($"Child1: {node.Child1.Id}");
+                LowestCommonAncestor(node.Child1, NodeLeft, NodeRight);
+            }
 
-        
-        return new Node();
+            if (node.Child2 is not null)
+            {
+                Console.WriteLine($"Child2: {node.Child2.Id}");
+                LowestCommonAncestor(node.Child2, NodeLeft, NodeRight);
+            }
+        }
     }
 }
 
